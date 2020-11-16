0de15d6bf261d6405f55e02d15ccc00b5559aa83
franr99 <frosas@alu.ucam.edu>
2020-11-16 15:52:39 +0100

    primeros pasos

---
+++ Practicafinal/app/src/main/java/com/example/practicafinal/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.practicafinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final CheckBox fiebre = (CheckBox)findViewById(R.id.CbFiebre);
+        final CheckBox gusto = (CheckBox)findViewById(R.id.CbGusto);
+        final CheckBox cabeza = (CheckBox)findViewById(R.id.CbCabeza);
+        final CheckBox diarrea = (CheckBox)findViewById(R.id.CbDiarrea);
+        final CheckBox fatiga = (CheckBox)findViewById(R.id.CbFatiga);
+        final Button BtSintomas = (Button) findViewById(R.id.BtSintomas);
+
+        BtSintomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    if(fiebre.isChecked() && fatiga.isChecked()){
+                        posiblePositivo();
+                    }
+                    if(cabeza.isChecked() && fatiga.isChecked() && gusto.isChecked()){
+                        posiblePositivo();
+                    }
+                    if(diarrea.isChecked() && fatiga.isChecked() && gusto.isChecked()){
+                        posiblePositivo();
+                    }
+
+            }
+
+        });
+    }
+
+    public void posiblePositivo(){
+        Intent intent= new Intent(this,  Sintomas.class);
+        startActivity(intent);
+    }
+}
