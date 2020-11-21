b7067cc4ae45904b46af0b808990962ae7b503db
franr99 <frosas@alu.ucam.edu>
2020-11-21 15:06:22 +0100

    recomendaciones terminado

--- navDrawer/app/src/main/java/com/example/navdrawer/ui/Recomendaciones.java
+++ navDrawer/app/src/main/java/com/example/navdrawer/ui/Recomendaciones.java
@@ -1,6 +1,15 @@
 package com.example.navdrawer.ui;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +21,37 @@ public class Recomendaciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recomendaciones);
+        final ImageView youtube = findViewById(R.id.youtube);
+        final ImageView twitter = findViewById(R.id.twitter);
+        final ImageView internet = findViewById(R.id.internet);
+
+        youtube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.youtube.com/watch?v=ciUniZGD4tY&feature=youtu.be&ab_channel=WorldHealthOrganization%28WHO%29");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://twitter.com/who");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        internet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.who.int/es");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
