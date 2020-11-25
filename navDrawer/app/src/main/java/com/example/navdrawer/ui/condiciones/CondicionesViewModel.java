7df927521a812815bcb6b226fdc5d543c0809a61
JJbestOf99 <56269172+JJbestOf99@users.noreply.github.com>
2020-11-25 16:54:23 +0100

    nav_drawer

--- navDrawer/app/src/main/java/com/example/navdrawer/ui/condiciones/CondicionesViewModel.java
+++ navDrawer/app/src/main/java/com/example/navdrawer/ui/condiciones/CondicionesViewModel.java
@@ -1,14 +1,14 @@
-package com.example.navdrawer.ui.gallery;
+package com.example.navdrawer.ui.condiciones;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class CondicionesViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public CondicionesViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
     }
