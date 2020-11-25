3de7d5455910aec79cefc6b08155937f1be3582d
franr99 <frosas@alu.ucam.edu>
2020-11-25 16:49:34 +0100

    youtube

--- navDrawer/app/src/main/java/com/example/navdrawer/Restricciones.java
+++ navDrawer/app/src/main/java/com/example/navdrawer/Restricciones.java
@@ -11,24 +11,12 @@ import java.util.Vector;
 public class Restricciones extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    Vector<YoutubeVideos> youtubeVideos = new Vector<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.restricciones);
-
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        youtubeVideos.add(new YoutubeVideos("<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/Jgu6DVjCb8s&ab_channel=RTVENoticias\" frameborder=\"0\" allowfullscreen></iframe>"));
-
-        VideoAdapter videoAdapter = new VideoAdapter(youtubeVideos);
-
-        recyclerView.setAdapter(videoAdapter);
-
-
 
     }
 }
--- navDrawer/app/src/main/java/com/example/navdrawer/YoutubeVideos.java
+++ navDrawer/app/src/main/java/com/example/navdrawer/YoutubeVideos.java
@@ -1,13 +1,38 @@
 package com.example.navdrawer;
 
-public class YoutubeVideos {
-    String video;
+import android.os.Bundle;
 
-    public YoutubeVideos(){
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class YoutubeVideos extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    YouTubePlayerView yt;
+    String clave = "AIzaSyDpT-ZX0A5k18DQ3Rwsr2KmZpYXswvWAUs";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video_view);
+        yt = (YouTubePlayerView)findViewById(R.id.youtube);
+        yt.initialize(clave,this);
     }
 
-    public YoutubeVideos(String video){ this.video = video; }
-    public String getVideo(){ return video; }
-    public void setVideo(String video){ this.video = video;}
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        if(!b){
+            youTubePlayer.cueVideo("Jgu6DVjCb8s");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+
+            youTubeInitializationResult.getErrorDialog(this,1).show();
+        }
+    }
 }
